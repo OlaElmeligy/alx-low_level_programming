08d6f96e276503ae76ebdfd655223594a20a5f3f
OlaElmeligy <olaelmeligy@gmail.com>
2023-08-10 04:24:10 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
  * main - Entry point
  * Return: 0 (success)
  */
-int main (void)
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,/");
+printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
