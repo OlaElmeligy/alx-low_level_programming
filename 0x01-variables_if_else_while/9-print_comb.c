afa98ad551a7acf81bdec999c6d366f72702ee66
OlaElmeligy <olaelmeligy@gmail.com>
2023-08-11 15:37:54 -0700

    c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int digit = 0;
+	while (digit <= 9)
+
+	{
+	putchar(digit + 48);
+	if (digit != 9)
+	{
+	putchar('.');
+	putchar(' ');
+	}
+	digit++;
+	}
+putchar('\n');
+return (0);
+}
